dc3cf7da52b78b379e2e590fb4e7230bcb149d03
ajibagus <ajibagus48@gmail.com>
2022-10-19 17:17:43 +0700

    user model

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -14,7 +14,7 @@ class UserModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['email','username','password'];
+    protected $allowedFields    = ['email','username','password','profil'];
 
     // Dates
     protected $useTimestamps = false;
